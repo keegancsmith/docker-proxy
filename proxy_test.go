dcb18c084f4ede95db25154538c2d450c6315bba
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2016-01-11 20:01:31 +0200

    Added TestUnixSocketTLSReverseProxy

--- proxy_test.go
+++ proxy_test.go
@@ -34,11 +34,70 @@ func TestUnixSocketReverseProxy(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	res.Body.Close()
 	if res.StatusCode != 404 {
 		t.Fatal("Fetching top-level URL should 404")
 	}
 
 	res, err = http.Get(ts.URL + "/helloworld")
+	if err != nil {
+		t.Fatal(err)
+	}
+	greeting, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(greeting) != "Hello World" {
+		t.Fatal("Response body not Hello World", string(greeting))
+	}
+}
+
+func TestUnixSocketTLSReverseProxy(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { os.RemoveAll(dir) }()
+
+	sockPath := filepath.Join(dir, "docker.sock")
+	cleanup, err := serveUnixHandler(sockPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cleanup()
+
+	serverCert, err := getOrGenerateServerCert(dir, []string{"127.0.0.1"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	tlsConfig, err := serverCert.TLSConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Then setup proxy and see if we successfully connect to the socket
+	proxy := UnixSocketReverseProxy(sockPath)
+	ts := httptest.NewUnstartedServer(proxy)
+	ts.TLS = tlsConfig
+	ts.StartTLS()
+	defer ts.Close()
+
+	httpTransport := &http.Transport{
+		TLSClientConfig: tlsConfig,
+	}
+	client := http.Client{Transport: httpTransport}
+
+	res, err := client.Get(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res.Body.Close()
+	if res.StatusCode != 404 {
+		t.Fatal("Fetching top-level URL should 404")
+	}
+
+	res, err = client.Get(ts.URL + "/helloworld")
 	if err != nil {
 		t.Fatal(err)
 	}
